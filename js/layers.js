2d25a0ebf8fde6d3db419e90d16702612b256722
hhrjfjjxg <ryan.tttom@gmail.com>
2026-09-02 19:12:28 -0400

    new things

--- js/layers.js
+++ js/layers.js
@@ -66,7 +66,7 @@ addLayer("s", {
     }},
     branches: ["o"],
     color: "#3ea03e",
-    requires: new Decimal(1e3), // Can be a function that takes requirement increases into account
+    requires: new Decimal(1e4), // Can be a function that takes requirement increases into account
     resource: "Oak Saplings", // Name of prestige currency
     baseResource: "Oak Logs", // Name of resource prestige is based on
     baseAmount() {return player.o.points}, // Get the current amount of baseResource
@@ -84,4 +84,46 @@ addLayer("s", {
         {key: "s", description: "S: Reset for Oak Saplings", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
+    upgrades: {
+11: {
+    title: "saplings scale up leaves",
+    description: "basic logic",
+    cost: new Decimal(1),
+        effect() {
+        return player[this.layer].points.add(1).pow(0.5)
+    },
+    effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
+    }
+})
+addLayer("p", {
+    name: "Pickaxe", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 4, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    branches: ["s"],
+    color: "#5c3617",
+    requires: new Decimal(1e4), // Can be a function that takes requirement increases into account
+    resource: "", // Name of prestige currency
+    baseResource: "Oak Saplings", // Name of resource prestige is based on
+    baseAmount() {return player.s.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for pickaxes", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+    }
 })
